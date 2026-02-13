ea8dda898fbf7810ef86345ea89158db0283c922
Juan Zuluaga <juan.zuluaga@empactit.com>
2026-02-12 19:06:20 -0500

    Basic ejercice 1

---
+++ LogicConcepts/NumerIsOddOrNot/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese número: ");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);    
+if (numberInt %  2 == 0)
+{
+    Console.WriteLine($"El número: {numberInt} es par");
+}
+else
+{
+    Console.WriteLine($"El número: {numberInt} es impar");
+}
